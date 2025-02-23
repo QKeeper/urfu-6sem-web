155175480c0ee0795abe407afae2ba213c6b2bc7
QKeeper <moncer256710@gmail.com>
2025-02-23 18:18:50 +0500

    Added 'Task' model to prisma

--- server/src/models/auth.model.ts
+++ server/src/models/auth.model.ts
@@ -7,8 +7,8 @@ export interface ITokenPayload {
 export enum Permission {
   VIEW_SITE = 1 << 0, // 1
   EDIT_CONTENT = 1 << 1, // 2
-  ACCESS_ADMIN_PANEL = 1 << 2, // 3
-  VIEW_ANALYTICS = 1 << 3, // 4
-  MANAGE_GLOBAL_SETTINGS = 1 << 4, // 5
-  MANAGE_USERS = 1 << 5, // 6
+  ACCESS_ADMIN_PANEL = 1 << 2, // 4
+  VIEW_ANALYTICS = 1 << 3, // 8
+  MANAGE_GLOBAL_SETTINGS = 1 << 4, // 16
+  MANAGE_USERS = 1 << 5, // 32
 }
